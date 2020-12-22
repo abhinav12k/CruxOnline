7c60b76d801adc02dd84502bdd61152c4bfa285d
abhinav <abhi2000.rtk@gmail.com>
2020-12-22 23:55:24 +0530

    Graph basic implementation

---
+++ Graph/Graph.java
@@ -0,0 +1,110 @@
+package CruxOnline.Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+
+	private class Vertex {
+		HashMap<String, Integer> nbrs = new HashMap<>();
+	}
+
+	private HashMap<String, Vertex> vtcs;
+
+	public Graph() {
+		vtcs = new HashMap<>();
+	}
+
+	public int numVertices() {
+		return this.vtcs.size();
+	}
+
+	public boolean containsVertex(String vname) {
+		return this.vtcs.containsKey(vname);
+	}
+
+	public void removeVertex(String vname) {
+
+		Vertex vtx = this.vtcs.get(vname);
+		ArrayList<String> nbrKeys = new ArrayList<>(vtx.nbrs.keySet());
+
+		for (String key : nbrKeys) {
+			Vertex nbrVtx = this.vtcs.get(key);
+			nbrVtx.nbrs.remove(vname);
+		}
+
+		this.vtcs.remove(vname);
+
+	}
+
+	public void addVertex(String vname) {
+		Vertex vtx = new Vertex();
+		this.vtcs.put(vname, vtx);
+	}
+
+	public int numEdges() {
+		ArrayList<String> vertices = new ArrayList<>(vtcs.keySet());
+		int count = 0;
+		for (String vtx : vertices) {
+			count += this.vtcs.get(vtx).nbrs.size();
+		}
+		return count / 2;
+	}
+
+	public boolean containsEdge(String vname1, String vname2) {
+
+		// If vname1 contains vname2 as nbr and vname2 contains vname1 as nbr then both
+		// contains an edge in between
+		Vertex v1 = this.vtcs.get(vname1);
+		Vertex v2 = this.vtcs.get(vname2);
+
+		if (v1 == null || v2 == null || !v1.nbrs.containsKey(vname2))
+			return false;
+
+		return true;
+
+	}
+
+	public void removeEdge(String vname1, String vname2) {
+
+		Vertex v1 = this.vtcs.get(vname1);
+		Vertex v2 = this.vtcs.get(vname2);
+
+		if (v1 == null || v2 == null || !v1.nbrs.containsKey(vname2))
+			return;
+
+		v1.nbrs.remove(vname2);
+		v2.nbrs.remove(vname1);
+
+	}
+
+	public void addEdge(String vname1, String vname2, int cost) {
+
+		Vertex v1 = this.vtcs.get(vname1);
+		Vertex v2 = this.vtcs.get(vname2);
+
+		if (v1 == null || v2 == null || v1.nbrs.containsKey(vname2))
+			return;
+
+		v1.nbrs.put(vname2, cost);
+		v2.nbrs.put(vname1, cost);
+
+	}
+
+	public void display() {
+
+		System.out.println("---------------------");
+
+		ArrayList<String> keys = new ArrayList<>(this.vtcs.keySet());
+
+		for (String key : keys) {
+
+			Vertex vtx = this.vtcs.get(key);
+			System.out.println(key + " : " + vtx.nbrs);
+
+		}
+		System.out.println("---------------------");
+
+	}
+
+}
---
+++ Graph/GraphClient.java
@@ -0,0 +1,48 @@
+package CruxOnline.Graph;
+
+public class GraphClient {
+
+	public static void main(String[] args) {
+		
+		Graph graph = new Graph();
+		
+		graph.addVertex("A");
+		graph.addVertex("B");
+		graph.addVertex("C");
+		graph.addVertex("D");
+		graph.addVertex("E");
+		graph.addVertex("F");
+		graph.addVertex("G");
+		
+		graph.addEdge("A", "B", 2);
+		graph.addEdge("A", "D", 15);
+		graph.addEdge("B", "C", 1);
+		graph.addEdge("C", "D", 6);
+		graph.addEdge("D", "E", 9);
+		graph.addEdge("E", "F", 16);
+		graph.addEdge("E", "G", 12);
+		graph.addEdge("F", "G", 8);
+		
+		graph.display();
+		
+		System.out.println(graph.numEdges());
+		System.out.println(graph.numVertices());
+		
+		System.out.println(graph.containsEdge("A", "D"));
+		System.out.println(graph.containsEdge("A", "F"));
+		
+		graph.removeEdge("A", "B");
+		graph.display();
+		
+		System.out.println(graph.containsVertex("G"));
+		System.out.println(graph.containsVertex("H"));
+		
+		graph.addEdge("A", "G", 100);
+		graph.display();
+		
+		graph.removeVertex("G");
+		graph.display();
+		
+	}
+	
+}
