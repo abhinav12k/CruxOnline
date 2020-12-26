0e26cea9860532c095f3b529d4bdbe2dc9cff55d
abhinav <abhi2000.rtk@gmail.com>
2020-12-26 17:08:14 +0530

    AVL Trees

---
+++ AVL/AVL_Client.java
@@ -0,0 +1,22 @@
+package CruxOnline.AVL;
+
+public class AVL_Client {
+
+	public static void main(String[] args) {
+		
+		AVL_Tree avl = new AVL_Tree();
+		avl.insert(20);
+		avl.insert(25);
+		avl.insert(30);
+		avl.insert(10);
+		avl.insert(5);
+		avl.insert(15);
+		avl.insert(27);
+		avl.insert(19);
+		avl.insert(16);
+		avl.display();
+		
+		
+	}
+	
+}
